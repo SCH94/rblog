1e141a60bbb542d8d341be77e7a9dbd25e2fe879
sch94 <arul1694@gmail.com>
2018-04-15 14:37:36 +0545

    Added new, create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,29 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
 	def new
+		@article = Article.new
+	end
+	def create
+=begin
+		@article = Article.new
+		@article.title = params[:article][:title]
+		@article.body = params[:article][:body]
+=end
+
+=begin
+	
+        @article = Article.new(
+                title: params[:article][:title],
+                body: params[:article][:body])
+=end
+       
+       # @article = Artical.new(params[:article])
+		@article=Article.new(article_params)
+		@article.save
+		redirect_to article_path(@article.id)
 	end
 	def show
 		@article = Article.find(params[:id])

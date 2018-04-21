6e34469a0a27190cfda280e74021a9cf915b48a2
sch94 <arul1694@gmail.com>
2018-04-21 18:18:23 +0545

    Tagging workflow added

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   	resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
+  resources :tags
 end
